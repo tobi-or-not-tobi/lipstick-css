a1093ddcd5bc9e3c023ea8444daf06404b96f4fa
Tobias Ouwejan <tobiasouwejan@gmail.com>
2018-05-23 23:19:43 +0200

    Add sample banner component to library

--- projects/sample-components/src/public_api.ts
+++ projects/sample-components/src/public_api.ts
@@ -1,7 +1,5 @@
 /*
  * Public API Surface of sample-components
  */
-
-export * from './lib/sample-components.service';
-export * from './lib/sample-components.component';
-export * from './lib/sample-components.module';
+export * from './lib/banner/banner.module';
+export * from './lib/banner/banner.component';
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BannerModule } from 'sample-components';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BannerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
