67acf8029089b8479dda6b60fb65248a84ee05c4
Tobias Ouwejan <tobiasouwejan@gmail.com>
2018-06-01 17:11:12 +0200

    Move CSS custom properties to library

--- src/app/css-var-config/css-var-config.component.ts
+++ src/app/css-var-config/css-var-config.component.ts
@@ -2,98 +2,93 @@ import { Component, OnInit } from '@angular/core';
 import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
-    selector: 'app-css-var-config',
-    templateUrl: './css-var-config.component.html',
-    styleUrls: ['./css-var-config.component.scss']
+  selector: 'app-css-var-config',
+  templateUrl: './css-var-config.component.html',
+  styleUrls: ['./css-var-config.component.scss']
 })
 export class CssVarConfigComponent implements OnInit {
-    primaryColor;
-    secondaryColor;
-    fontFamily;
-    fonts = [
-        'Karla',
-        'Lora',
-        'Frank Ruhl Libre',
-        'Playfair Display',
-        'Archivo',
-        'Spectral',
-        'Fjalla One',
-        'Roboto',
-        'Montserrat',
-        'Rubik',
-        'Source Sans',
-        'Cardo',
-        'Cormorant',
-        'Yatra One',
-        'Arvo',
-        'Lato'
-    ];
-    borderRadius;
+  primaryColor;
+  secondaryColor;
+  fontFamily;
+  fonts = [
+    'Karla',
+    'Lora',
+    'Frank Ruhl Libre',
+    'Playfair Display',
+    'Archivo',
+    'Spectral',
+    'Fjalla One',
+    'Roboto',
+    'Montserrat',
+    'Rubik',
+    'Source Sans',
+    'Cardo',
+    'Cormorant',
+    'Yatra One',
+    'Arvo',
+    'Lato'
+  ];
+  borderRadius;
 
-    fontUrl;
+  fontUrl;
 
-    constructor(public sanitizer: DomSanitizer) {}
+  constructor(public sanitizer: DomSanitizer) {}
 
-    ngOnInit() {
-        this.primaryColor = this.getCssVar('--primary');
-        this.secondaryColor = this.getCssVar('--secondary');
-        this.fontFamily = this.getCssVar('--font');
-        this.fontFamily = this.getCssVar('--font');
-        this.borderRadius = this.getCssVar('--radius');
+  ngOnInit() {
+    this.primaryColor = this.getCssVar('--primary-color');
+    this.secondaryColor = this.getCssVar('--secondary-color');
+    this.fontFamily = this.getCssVar('--font');
+    this.fontFamily = this.getCssVar('--font');
+    this.borderRadius = this.getCssVar('--radius');
+  }
+
+  save(event) {
+    if (this.primaryColor) {
+      document.documentElement.style.setProperty('--primary-color', this.primaryColor);
+      document.documentElement.style.setProperty(`--primary-darken`, this.lightenColor(this.primaryColor, -15));
+      console.log(this.getCssVar('--primary-color'));
     }
-
-    save(event) {
-        if (this.primaryColor) {
-            document.documentElement.style.setProperty('--primary-color', this.primaryColor);
-            document.documentElement.style.setProperty(
-                `--primary-darken-color`,
-                this.lightenColor(this.primaryColor, -15)
-            );
-        }
-        if (this.secondaryColor) {
-            document.documentElement.style.setProperty('--secondary-color', this.secondaryColor);
-            document.documentElement.style.setProperty(
-                `--secondary-darken-color`,
-                this.lightenColor(this.secondaryColor, -15)
-            );
-        }
-        if (this.fontFamily) {
-            this.fontUrl = `https://fonts.googleapis.com/css?family=${this.fontFamily}`;
-            document.documentElement.style.setProperty('--font-family', this.fontFamily);
-        }
-        if (this.borderRadius) {
-            document.documentElement.style.setProperty('--border-radius', this.borderRadius);
-        }
+    if (this.secondaryColor) {
+      document.documentElement.style.setProperty('--secondary-color', this.secondaryColor);
+      document.documentElement.style.setProperty(`--secondary-darken`, this.lightenColor(this.secondaryColor, -15));
     }
-
-    getCssVar(name: string) {
-        const bodyStyles = window.getComputedStyle(document.body);
-        let value = bodyStyles.getPropertyValue(name).trim();
-        // in case this is a string, we need to get ride of the quotes
-        value = value.replace(/"/g, '');
-        return value;
+    if (this.fontFamily) {
+      this.fontUrl = `https://fonts.googleapis.com/css?family=${this.fontFamily}`;
+      document.documentElement.style.setProperty('--font-family', this.fontFamily);
     }
-
-    private lightenColor(color, percent) {
-        color = color.trim().slice(1);
-
-        var num = parseInt(color, 16),
-            amt = Math.round(2.55 * percent),
-            R = (num >> 16) + amt,
-            B = ((num >> 8) & 0x00ff) + amt,
-            G = (num & 0x0000ff) + amt;
-        if (!num) {
-            console.error('could not transform the color', color, num);
-            return color;
-        }
-        const value = (
-            0x1000000 +
-            (R < 255 ? (R < 1 ? 0 : R) : 255) * 0x10000 +
-            (B < 255 ? (B < 1 ? 0 : B) : 255) * 0x100 +
-            (G < 255 ? (G < 1 ? 0 : G) : 255)
-        )
-            .toString(16)
-            .slice(1);
-        return '#' + value;
+    if (this.borderRadius) {
+      document.documentElement.style.setProperty('--border-radius', this.borderRadius);
     }
+  }
+
+  getCssVar(name: string) {
+    const bodyStyles = window.getComputedStyle(document.body);
+    let value = bodyStyles.getPropertyValue(name).trim();
+    // in case this is a string, we need to get ride of the quotes
+    value = value.replace(/"/g, '');
+    return value;
+  }
+
+  private lightenColor(color, percent) {
+    color = color.trim().slice(1);
+
+    var num = parseInt(color, 16),
+      amt = Math.round(2.55 * percent),
+      R = (num >> 16) + amt,
+      B = ((num >> 8) & 0x00ff) + amt,
+      G = (num & 0x0000ff) + amt;
+    if (!num) {
+      console.error('could not transform the color', color, num);
+      return color;
+    }
+    const value = (
+      0x1000000 +
+      (R < 255 ? (R < 1 ? 0 : R) : 255) * 0x10000 +
+      (B < 255 ? (B < 1 ? 0 : B) : 255) * 0x100 +
+      (G < 255 ? (G < 1 ? 0 : G) : 255)
+    )
+      .toString(16)
+      .slice(1);
+    return '#' + value;
+  }
 }
